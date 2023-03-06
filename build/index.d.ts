02102a190ca28d2099608995120f42d1353da859
Titus Meader <titusmeader@btinternet.com>
2023-03-06 17:26:00 +0000

    asdlijuhsdiugyfiuysdafgya

--- build/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare function dashAuthenticate(email: string, password: string): Promise<import("@xboxreplay/xboxlive-auth").AuthenticateResponse>;
-export { dashAuthenticate };
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { authenticate } from '@xboxreplay/xboxlive-auth';
-
-function dashAuthenticate(email: string, password: string) {
-	try { return authenticate(email, password, { XSTSRelyingParty: "https://pocket.realms.minecraft.net/" }); } catch (e) { throw e; }
-}
-
-export { dashAuthenticate }
