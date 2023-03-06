6cd50db5d49a4840e96b2df72441d1a9a3f91f5f
Titus Meader <titusmeader@btinternet.com>
2023-03-06 19:22:17 +0000

    fix

--- build/index.js
+++ build/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dashAuthenticate = void 0;
-var xboxlive_auth_1 = require("@xboxreplay/xboxlive-auth");
+const xboxlive_auth_1 = require("@xboxreplay/xboxlive-auth");
 function dashAuthenticate(email, password) {
     try {
         return (0, xboxlive_auth_1.authenticate)(email, password, { XSTSRelyingParty: "https://pocket.realms.minecraft.net/" });
